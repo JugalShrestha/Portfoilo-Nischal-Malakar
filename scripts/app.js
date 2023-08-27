0b47404876e122faed95abbcd4bf135a00d2bf94
Jugal Shrestha <jugalnepali@gmail.com>
2023-08-27 21:00:43 +0545

    Add: Skills and Services expanded

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,3 @@
-
-
 function checkScreenSize(){
     {
         const observer = new IntersectionObserver(entities=>{
@@ -24,9 +22,37 @@ function checkScreenSize(){
 
 checkScreenSize();
 
-window.addEventListener('load',()=>{
-    const deviceSize = window.innerWidth;
-    const heroPageText = document.querySelector('#hero-page .text .main-text')
-    if(deviceSize<980){
+//For animating NavBar on scroll
+
+function ChangeOnScroll(){
+    const navBar = document.querySelector('.nav-bar');
+    const navOptions = document.querySelectorAll(".nav-option");
+    const scrolledPos = window.scrollY;
+
+    if(scrolledPos > navBar.clientHeight)
+    {
+        navBar.style.padding = "1vh 4vh";
+        navBar.style.background = "rgba(255,255,255,0.5)";
+        navOptions.forEach(option => {
+            option.style.color = "var(--p1-color)";
+        })
     }
+    else{
+        navBar.style.padding = "3vh 4vh";
+        navBar.style.background = "var(--p1-color)"
+        navOptions.forEach(option => {
+            option.style.color = "var(--s1-color)";
+        })
+    }
+}
+
+window.addEventListener('scroll',ChangeOnScroll);
+
+const navOptions = document.querySelectorAll('.nav-option')
+const pages = document.querySelectorAll('.pages')
+
+navOptions.forEach((option,index)=>{
+    option.addEventListener("click",()=>{
+        pages[index].scrollIntoView({behavior: "smooth"})
+    })
 })
